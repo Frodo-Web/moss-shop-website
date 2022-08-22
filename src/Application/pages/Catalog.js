794a5c3159b4973b804b7061daaf86810b4e87f5
Frodo <102017064+Frodo-Web@users.noreply.github.com>
2022-08-22 16:34:16 +0300

    Basic catalog page

--- src/Application/pages/Catalog.js
+++ src/Application/pages/Catalog.js
@@ -3,10 +3,83 @@ import './Catalog.css';
 
 const Catalog = () => {
 	return (
-		<>
-			<h2>Catalog</h2>
-		</>
+		<div className='catalog'>
+			<Grid />
+		</div>
 	)
 };
 
 export default Catalog;
+
+const Grid = () => {
+
+	return (
+		<div className='grid'>
+			<Cards />
+		</div>
+	)
+}
+
+const Cards = () => {
+
+	return (
+		<>
+			<div className='card'>
+				<div></div>
+				<div>Мох в сотах</div>
+				<div>1,300 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Мох в Кашпо</div>
+				<div>1,000 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Соты с мхом</div>
+				<div>1,300 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Мох стабилизированный, Ягель</div>
+				<div>2,000 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Композиция из мха</div>
+				<div>3,000 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Чёрный квадрат с мхом</div>
+				<div>1,300 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Панно Семья</div>
+				<div>8,000 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Папоротник</div>
+				<div>130 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Эвкалипт бэби, стабилизированный</div>
+				<div>450 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Эвкалипт стабилизированный</div>
+				<div>300 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Мох пластовый, стабилизированный</div>
+				<div>300 ₽</div>
+			</div>
+
+		</>
+	)
+}
