e37392f1fb1731b23ce4cf718d7eddfb530cbf6e
Frodo <102017064+Frodo-Web@users.noreply.github.com>
2022-08-25 11:24:35 +0300

    Add services page

--- src/Application/pages/Catalog.js
+++ src/Application/pages/Catalog.js
@@ -27,67 +27,67 @@ const Cards = () => {
 			<div className='card'>
 				<div></div>
 				<div>Мох в сотах</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>1,300 ₽ <div className='line-through'>1,600 ₽</div></div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Мох в Кашпо</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>1,000 ₽</div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Соты с мхом</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>1,300 ₽ <div className='line-through'>1,600 ₽</div></div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Мох стабилизированный, Ягель</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>2,000 ₽ <div className='line-through'>3,000 ₽</div></div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Композиция из мха</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>3,000 ₽</div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Чёрный квадрат с мхом</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>1,300 ₽</div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Панно Семья</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>8,000 ₽</div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Папоротник</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>130 ₽ <div className='line-through'>200 ₽</div></div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Эвкалипт бэби, стабилизированный</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>450 ₽</div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Эвкалипт стабилизированный</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>300 ₽</div>
 			</div>
 			<div className='card'>
 				<div></div>
 				<div>Мох пластовый, стабилизированный</div>
-				<button>Добавить в корзину</button>
+				<button>Подробнее</button>
 				<div>300 ₽</div>
 			</div>
 
--- src/Application/pages/Service.js
+++ src/Application/pages/Service.js
@@ -3,10 +3,39 @@ import './Service.css';
 
 const Service = () => {
 	return (
-		<>
-			<h2>Сервис</h2>
-		</>
+		<div className='service'>
+			<Grid />
+		</div>
 	)
 };
 
 export default Service;
+
+const Grid = () => {
+
+	return (
+		<div className='grid'>
+			<Cards />
+		</div>
+	)
+}
+
+const Cards = () => {
+
+	return (
+		<>
+			<div className='card'>
+				<div></div>
+				<div>Озеленение помещений искусственными растениями</div>
+				<button>Подробнее</button>
+				<div>18,000 ₽</div>
+			</div>
+			<div className='card'>
+				<div></div>
+				<div>Фито дизайн интерьера</div>
+				<button>Подробнее</button>
+				<div>10,000 ₽ за m²</div>
+			</div>
+		</>
+	)
+}
