cc0b354916884a3cd811f5a14d2ddb6aba422dbc
Frodo <102017064+Frodo-Web@users.noreply.github.com>
2022-10-24 19:41:05 +0300

    Starting developing logger with winston

--- server/web.js
+++ server/web.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const config = require('config');
 const express = require('express');
 const historyAPIfallback = require("./middleware/historyAPIfallback");
+const logger = require("../lib/logger");
+
 const app = express();
-const port = process.env.PORT || 80;
+const port = config.get("port") || 80;
 
 
 app.use(express.static(path.join(__dirname, '../docs')));
@@ -11,6 +14,7 @@ app.use(express.json());
 
 app.use('/', require('./routes/index'));
 
-app.listen(port, function() {
+app.listen(port, function () {
 	console.log(`listening on port ${port}`);
+	logger.info(`listening on port ${port}`);
 });
